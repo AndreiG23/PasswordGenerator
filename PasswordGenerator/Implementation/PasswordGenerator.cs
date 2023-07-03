c663fb8bedf1cad11cafe93ebf414cd545c08472
Ghiurcuta Andrei <ghiurcutaandrei@gmail.com>
2023-07-03 15:38:43 +0300

    Initial commit

---
+++ PasswordGenerator/Implementation/PasswordGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Implementation
+{
+    internal class OTPGenerator
+    {
+        private const int ValidityPeriod = 30;
+
+        public static string GenerateOneTimePassword(string userId, DateTime dateTime)
+        {
+            string seed = $"{userId}{dateTime.ToString("yyyyMMddHHmmss")}";
+
+            string hash = seed.GetHashCode().ToString();
+
+            string password = hash.Substring(0, 8);
+
+            return password;
+        }
+
+        public static bool IsPasswordValid(string password, DateTime dateTime)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            TimeSpan elapsedTime = currentDateTime - dateTime;
+
+            return elapsedTime.TotalSeconds <= ValidityPeriod;
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using PasswordGenerator.Implementation;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userId = "user123";
+        DateTime dateTime = DateTime.Now;
+        string otp = OTPGenerator.GenerateOneTimePassword(userId, dateTime);
+
+        Console.WriteLine("Generated one-time password: " + otp);
+
+        bool isValid = OTPGenerator.IsPasswordValid(otp, dateTime);
+
+        Console.WriteLine("Is password valid? " + isValid);
+    }
+}
